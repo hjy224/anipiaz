5335772069e70fe518fa3c6956e9da54de1d3508
hjy224 <hjy224@allwin.co.kr>
2022-02-22 17:25:48 +0900

    2

--- alphaseat/resource/js/script.js
+++ alphaseat/resource/js/script.js
@@ -1,18 +1,18 @@
-    /*gnb 팝업 정의*/
-  $(document).on("click", "#btn_gnb", function () {
-      $("#ags-wrap").addClass("gnb-active");
-      $("#gnb-2dp").addClass("active");
-      $("html, body").css("overflow", "hidden");
-  });
-  $(document).on("click", "#btn_gnb-close", function () {
-      $("#gnb-2dp").removeClass("active");
-      setTimeout(function () { $("#ags-wrap").removeClass("gnb-active"); }, 1000);
-      $("html, body").css("overflow", "");
-  });
+/*gnb 팝업 정의*/
+$(document).on("click", "#btn_gnb", function () {
+  $("#ags-wrap").addClass("gnb-active");
+  $("#gnb-2dp").addClass("active");
+  $("html, body").css("overflow", "hidden");
+});
+$(document).on("click", "#btn_gnb-close", function () {
+  $("#gnb-2dp").removeClass("active");
+  setTimeout(function () {
+    $("#ags-wrap").removeClass("gnb-active");
+  }, 1000);
+  $("html, body").css("overflow", "");
+});
 
-
-
-
+/*모달팝업*/
 $('.btn_footer_open').click(function () {
   $('.ags-summary').addClass('modal-open');
 });
@@ -22,12 +22,22 @@ $('.btn_footer_close').click(function () {
 });
 
 
+ 
+/*스케일*/
+$( document ).ready(function() {
+  $('.trigger').on('click', function() {
+    $('.contents').toggleClass('scale');
+     return false;
+  });
+});
+
+
 /*배경 DIM 생성*/
 function createDim(target) {
   if (target.attr("data-type") === "drawer" || target.attr("data-type") === "message") {
-      $("#ags-wrap").append("<div class='page-dim " + target.attr("id") + " active'></div>"); 
+    $("#ags-wrap").append("<div class='page-dim " + target.attr("id") + " active'></div>");
   } else {
-      target.prepend("<div class='page-dim " + target.attr("id") + " active'></div>");
+    target.prepend("<div class='page-dim " + target.attr("id") + " active'></div>");
   }
   $("html, body").css("overflow", "hidden");
 }
